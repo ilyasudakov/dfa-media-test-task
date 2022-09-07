2b2db7c8bfeed5d5f06928c88ff87f38c48e7fb2
ilyasudakov <ilyasudakov.dev@gmail.com>
2022-09-07 17:05:17 +0300

    Add SidemenuMobile, hamburger menu button in Header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+
 import Button from "../Button";
+import SidemenuMobile from "./SidemenuMobile";
+
+import CrossIcon from "../../assets/cross.svg";
 
 export default function Header() {
+  const { width } = useWindowDimensions();
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <header className="h-[100px] flex fixed top-0 bg-bg-dark lg:px-10">
-      <ul className="flex gap-4 text-[0.6rem] items-center md:pl-[200px]">
-        <li>Мероприятия</li>
+    <header className="h-[100px] flex items-center fixed top-0 bg-bg-dark px-4 lg:px-10">
+      {showMenu && <MobileMenu hideMenu={() => setShowMenu(false)} />}
+      {width && width < 1024 && (
+        <div
+          onClick={() => setShowMenu(true)}
+          className="flex flex-col gap-[6px] cursor-pointer p-1"
+        >
+          <span className="block w-[30px] h-[2px] bg-secondary-light-grey"></span>
+          <span className="block w-[30px] h-[2px] bg-secondary-light-grey"></span>
+          <span className="block w-[30px] h-[2px] bg-secondary-light-grey"></span>
+        </div>
+      )}
+      <ul className="flex gap-4 text-[0.6rem] items-center px-6 lg:pl-[200px]">
+        <li className="cursor-pointer">Мероприятия</li>
         <li>
           <Button>Войти</Button>
         </li>
@@ -12,3 +32,16 @@ export default function Header() {
     </header>
   );
 }
+
+const MobileMenu = ({ hideMenu }: { hideMenu: () => void }) => {
+  return (
+    <div className="flex flex-col p-4 fixed top-0 left-0 bg-bg-dark w-screen h-screen z-50">
+      <div className="cursor-pointer p-2 ml-auto" onClick={hideMenu}>
+        <CrossIcon />
+      </div>
+      <div className="flex mt-4">
+        <SidemenuMobile />
+      </div>
+    </div>
+  );
+};
--- src/components/Layout/SidemenuMobile.tsx
+++ src/components/Layout/SidemenuMobile.tsx
@@ -1,6 +1,6 @@
 import SidemenuList from "./SidemenuList";
 
-export default function SidemenuDesktop() {
+export default function SidemenuMobile() {
   return (
     <nav className="text-[0.6rem] ">
       <SidemenuList />
