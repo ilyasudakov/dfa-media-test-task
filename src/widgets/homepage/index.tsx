0483a74bac3e1a3aae3427b6033e8d6d1fe45167
ilyasudakov <ilyasudakov.dev@gmail.com>
2022-09-07 18:45:45 +0300

    Reduce amount of cards

--- src/widgets/homepage/index.tsx
+++ src/widgets/homepage/index.tsx
@@ -3,14 +3,14 @@ import Card from "./components/Card";
 export default function Homepage() {
   return (
     <div className="flex flex-wrap items-streth gap-4">
-      {Array.from({ length: 50 }).map((data, index) => (
+      {Array.from({ length: 10 }).map((data, index) => (
         <Card
           data={{
             level: 3,
             partnersNumber: 133,
           }}
           isActive={index === 0}
-          users={Array.from({ length: 20 })}
+          users={Array.from({ length: 10 })}
           key={index}
         />
       ))}
